ca070f82708d4680bfd7062ff87fca4ddcb0f2a6
Tom <tomaspere07@gmail.com>
2026-03-04 12:29:33 +0100

    añadido int fila y columna

---
+++ src/model/Pieza.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Pieza {
+
+    //las posiciones son del 0 al 7
+    protected int fila;
+    protected int columna;  //las posiciones son del 0 al 7, pero en el tablero serian A-H
+}
