9866c0720fa2a95f4bc76d94218bc0a93db327ad
Gabriel Raya <grayluq3003@g.educaand.es>
2026-03-05 14:17:06 +0100

    feature:Clase tablero vacia, solo con sus ArrayList declarados

---
+++ src/model/Tablero.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Tablero {
+    ArrayList<Pieza> piezasBlancas = new ArrayList<>();
+    ArrayList<Pieza> piezasNegras = new ArrayList<>();
+    ArrayList<Pieza> piezasEliminadas = new ArrayList<>();
+
+
+
+}
