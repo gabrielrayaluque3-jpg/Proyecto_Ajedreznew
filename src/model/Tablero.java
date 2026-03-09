24c69e3cb628cdb060be18361f2fab50481563d7
Tom <tomaspere07@gmail.com>
2026-03-09 13:56:07 +0100

    feature: add tablero utility methods and fix classes

--- src/model/Tablero.java
+++ src/model/Tablero.java
@@ -35,4 +35,63 @@ public class Tablero {
         return resultado;
     }
 
+    public void vaciarTablero(){
+        piezasBlancas.clear();
+        piezasNegras.clear();
+        piezasEliminadas.clear();
+    }
+
+    public Tablero crearCopiaTablero(){
+        Tablero copia = new Tablero();
+        for (Pieza pieza : piezasBlancas) {
+            copia.addPieza(pieza);
+        }
+        for (Pieza pieza : piezasNegras) {
+            copia.addPieza(pieza);
+        }
+        return copia;
+    }
+
+
+    public Pieza addPieza(String nombre,int fila, int columna, Color color) {
+        Pieza pieza = null;
+        switch (nombre) {
+            case "Reina":
+                pieza = new Reina(fila, columna, color);
+                break;
+            case "Caballo":
+                pieza = new Caballo(fila, columna, color);
+                break;
+            case "Alfil":
+                pieza = new Alfil(fila, columna, color);
+                break;
+            case "Torre":
+                pieza = new Torre(fila, columna, color);
+                break;
+            case "Peon":
+                pieza = new Peon(fila, columna, color);
+                break;
+            default:
+                throw new IllegalArgumentException("Nombre de pieza no válido: " + nombre);
+        }
+        return addPieza(pieza);
+    }
+
+    public Pieza getPieza(int fila, int columna) {
+        for (Pieza pieza : piezasBlancas) {
+            if (pieza.getFila() == fila && pieza.getColumna() == columna) {
+                return pieza;
+            }
+        }
+        for (Pieza pieza : piezasNegras) {
+            if (pieza.getFila() == fila && pieza.getColumna() == columna) {
+                return pieza;
+            }
+        }
+        return null;
+    }
+
+
+
+
 }
