e09df6897e342cba18a9e675d2c0fbab6fe31631
Tom <tomaspere07@gmail.com>
2026-03-12 11:14:05 +0100

    Revert "bugfix/metodo-movimiento-alfil"

--- src/model/Alfil.java
+++ src/model/Alfil.java
@@ -1,7 +1,5 @@
 package model;
 
-import utils.Utils;
-
 public class Alfil extends Pieza {
     final int puntos = 3;
 
@@ -11,10 +9,23 @@ public class Alfil extends Pieza {
 
     @Override
     public boolean movimiento(int nuevaFila, int nuevaColumna, Tablero tablero) {
-        if(Utils.esDiagonal(getFila(),getColumna(),nuevaFila,nuevaColumna)){
-            return true;
-        }
-        return false;
+        int distFila = nuevaFila - getFila();
+        int distCol = nuevaColumna - getColumna();
+        return distFila == distCol;
+    }
+
+    public int getPuntos() {
+        return 3;
+    }
+
+    @Override
+    public Pieza copiar() {
+        return null;
+    @Override
+    public boolean movimiento(int nuevaFila, int nuevaColumna) {
+        int distFila = nuevaFila - getFila();
+        int distCol = nuevaColumna - getColumna();
+        return distFila == distCol;
     }
 
     public int getPuntos() {
