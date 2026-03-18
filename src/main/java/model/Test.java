35a0ad9a2db6176e1ad573bf31b10ac445fc8d4a
gabrielraya <gabrielrayaluque3@gmail.com>
2026-03-18 14:18:58 +0100

    bugfix/

--- src/main/java/model/Test.java
+++ src/main/java/model/Test.java
@@ -1,43 +1,13 @@
 package model;
 
+import controller.TableroController;
 import model.*;
+import view.MenuPrincipal;
 
 public class  Test {
-    public static void main(String[] args) {
-        Tablero tablero = new Tablero();
-        tablero.reiniciarTablero();
-        System.out.println(tablero);
-        System.out.println("Reiniciando tablero...");
-        tablero.reiniciarTablero();
-        System.out.println("Tablero reiniciado:");
-        System.out.println(tablero);
-        System.out.println(tablero.estadoTablero());
-        try{
-            tablero.moverPieza("2b","3b");
-        }catch (MovimientoInvalido e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println(tablero);
-        try{
-            tablero.moverPieza("7e","5e");
-        }catch (MovimientoInvalido e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println(tablero);
-        try{
-            tablero.moverPieza("2d","4d");
-        }catch (MovimientoInvalido e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println(tablero);
-
-        try{
-            tablero.moverPieza("5e","4d");
-        }catch (MovimientoInvalido e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println(tablero);
-        System.out.println(tablero.estadoTablero());
+    static void main(String[] args) {
+        TableroController controller = new TableroController(new MenuPrincipal());
+        controller.menuFuncional();
     }
 }
 
